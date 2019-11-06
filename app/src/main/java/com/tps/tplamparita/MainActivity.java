c14117f3bacc7e230c38b22e339804acf7812e7a
dwloco <davidwis000@gmail.com>
2019-11-06 19:07:12 -0300

    lo probe en un android y funciona

--- app/src/main/java/com/tps/tplamparita/MainActivity.java
+++ app/src/main/java/com/tps/tplamparita/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tps.tplamparita;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
@@ -8,8 +10,6 @@ import android.media.SoundPool;
 import android.os.Build;
 import android.os.Handler;
 import android.support.annotation.RequiresApi;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -20,7 +20,7 @@ import android.widget.ImageView;
 import android.hardware.camera2.*;
 import android.widget.NumberPicker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     ImageView imgLamparita;
     ImageButton imgbtnInterruptor;
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     Runnable runnable = null;
     SoundPool sonidito;
     SharedPreferences preferencias;
+    Context context;
     int iSoundId;
     boolean bPrendido;
     boolean bLuzPrendida;
@@ -57,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         npkIntervalo.setMaxValue(20);
         sonidito = new SoundPool.Builder().setMaxStreams(10).build();
         iSoundId = sonidito.load(getApplicationContext(), R.raw.cebo, 1);
+        context = MainActivity.this;
         if (bTitilando) {
             npkIntervalo.setEnabled(true);
             chkParpadeo.setChecked(true);
@@ -88,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
             editor.commit();
             if (runnable != null) {
                 handler.removeCallbacks(runnable);
-                handler.postAtTime(runnable, System.currentTimeMillis()+iIntervalo*1000);
+                //handler.postAtTime(runnable, System.currentTimeMillis()+iIntervalo*1000);
                 handler.postDelayed(runnable, iIntervalo*1000);
             }
         }
@@ -105,75 +107,97 @@ public class MainActivity extends AppCompatActivity {
                     bLuzPrendida = true;
                     handler.removeCallbacks(runnable);
                     SetLuz(bLuzPrendida);
-                } else {
-                    runnable = new Runnable(){
-                        public void run() {
-                            bLuzPrendida = !bLuzPrendida;
-                            SetLuz(bLuzPrendida);
-                        }
-                    };
-                    handler.postAtTime(runnable, System.currentTimeMillis()+iIntervalo*1000);
-                    handler.postDelayed(runnable, iIntervalo*1000);
-                }
+                }/* else {
+                    establecerTitilado();
+                }*/
             } else {
                 bPrendido = false;
-                bTitilando = false;
+                //bTitilando = false;
                 bLuzPrendida = false;
                 handler.removeCallbacks(runnable);
-                SetLuz(bPrendido);
+                SetLuz(bLuzPrendida);
             }
-
-        }
-
-        @RequiresApi(api = Build.VERSION_CODES.M)
-        void SetLuz(boolean prendido) {
-            ReproducirSonido();
-            Context context = getApplicationContext();
-            if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)){
-                CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-                try {
-                    String cameraId = cameraManager.getCameraIdList()[0];
-                    cameraManager.setTorchMode(cameraId, prendido);
-                    int imagen = (prendido)? R.drawable.prendido : R.drawable.apagado;
-                    imgbtnInterruptor.setImageResource(imagen);
-                } catch (CameraAccessException e) {
-                    Log.d("Error", "No hay acceso a la camara");
-                }
-            }
+            int imagen = (bPrendido)? R.drawable.prendido : R.drawable.apagado;
+            imgbtnInterruptor.setImageResource(imagen);
         }
     };
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    void SetLuz(boolean prendido) {
+        ReproducirSonido();
+        Context context = getApplicationContext();
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)){
+            CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            try {
+                String cameraId = cameraManager.getCameraIdList()[0];
+                cameraManager.setTorchMode(cameraId, prendido);
+            } catch (CameraAccessException e) {
+                Log.d("Error", "No hay acceso a la camara");
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void establecerTitilado() {
+        runnable = new Runnable(){
+            public void run() {
+                bLuzPrendida = !bLuzPrendida;
+                SetLuz(bLuzPrendida);
+                //handler.postAtTime(this, System.currentTimeMillis()+iIntervalo*1000);
+                handler.removeCallbacks(runnable);
+                handler.postDelayed(this, iIntervalo*1000);
+            }
+        };
+        handler.removeCallbacks(runnable);
+        //handler.postAtTime(runnable, System.currentTimeMillis()+iIntervalo*1000);
+        handler.postDelayed(runnable, iIntervalo*1000);
+    }
 
     private void ReproducirSonido() {
         sonidito.play(iSoundId, 1, 1, 1, 0, 1);
     }
 
     CompoundButton.OnCheckedChangeListener chkparpadeo_changed = new CompoundButton.OnCheckedChangeListener() {
+        @RequiresApi(api = Build.VERSION_CODES.M)
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
             if (isChecked) {
-                AlertDialog mensajito = new AlertDialog.Builder(getApplicationContext())
+                AlertDialog mensajito = new AlertDialog.Builder(context)
                         .setTitle("Confirmar check")
                         .setMessage("Estas seguro que queres checkear este coso?")
                         .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            @RequiresApi(api = Build.VERSION_CODES.M)
                             public void onClick(DialogInterface dialog, int which) {
                                 bTitilando = true;
+                                if (bPrendido) {
+                                    establecerTitilado();
+                                }
                                 npkIntervalo.setEnabled(true);
                                 SharedPreferences.Editor editor = preferencias.edit();
                                 editor.putBoolean("CheckboxTitilando", true);
                                 editor.commit();
                             }
                         })
-                        .setNegativeButton(android.R.string.no, null)
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                chkParpadeo.setChecked(false);
+                            }
+                        })
                         .setIcon(android.R.drawable.ic_dialog_alert)
-                        .show();
+                        .create();
+                mensajito.show();
             } else {
+                handler.removeCallbacks(runnable);
                 bTitilando = false;
+                if (bPrendido) {
+                    SetLuz(true);
+                }
                 npkIntervalo.setEnabled(false);
                 SharedPreferences.Editor editor = preferencias.edit();
                 editor.putBoolean("CheckboxTitilando", false);
                 editor.commit();
             }
-
         }
     };
 }
